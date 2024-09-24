cfb92c3b513cdaa312f91db8231dcf7162f6f841
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-09-24 14:47:14 -0400

    Update 6_vcf_processing.sh

--- ProcessingAndQC/scRNA-seq_demux_doublets/6_vcf_processing.sh
+++ ProcessingAndQC/scRNA-seq_demux_doublets/6_vcf_processing.sh
@@ -3,19 +3,23 @@
 module load bcftools
 module load singularity
 
-# NECESSARY FOR DEMUXALOT (and maybe more softwares): 
-# sorting the VCF same as the BAM (only have to do this once--process one fastq w/ nf-core then do this, then can do the whole workflow)
+
+# sorting the VCF same as the BAM
 # have BAM, VCF, and script sort_vcf_same_as_bam.sh in the same directory, and run:
 ./sort_vcf_same_as_bam.sh ./possorted_genome_bam.bam ./my_vcf.vcf > ./my_vcf_reordered.vcf
-
-
-# rename chromosomes to match BAM w/ chr_map.txt
-bcftools annotate --rename-chrs chr_map.txt snRNA_SNPs_reordered_tagged.vcf -o snRNA_SNPs_reordered_tagged_chr.vcf
-bgzip -c snRNA_SNPs_reordered_tagged_chr.vcf > snRNA_SNPs_reordered_tagged_chr.vcf.bgz
-tabix -p vcf snRNA_SNPs_reordered_tagged_chr.vcf.bgz
 
 
 # add all "info" fields to the VCF (specifically need the "AF" field):
 bcftools +fill-tags my_vcf_reordered.vcf > my_vcf_reordered_tagged.vcf
 # view the tagged VCF: 
 bcftools view my_vcf_reordered_tagged.vcf | tail -5
+
+
+# rename chromosomes to match BAM w/ chr_map.txt
+# chr_map.txt should be a txt file w/ chromosome number and new name separated by a tab, 1 per line, e.g.:
+# 1  chr1
+# 2  chr2
+# etc.
+bcftools annotate --rename-chrs chr_map.txt my_vcf_reordered_tagged.vcf -o my_vcf_reordered_tagged_chr.vcf
+bgzip -c my_vcf_reordered_tagged_chr.vcf > my_vcf_reordered_tagged_chr.vcf.bgz
+tabix -p vcf my_vcf_reordered_tagged_chr.vcf.bgz
