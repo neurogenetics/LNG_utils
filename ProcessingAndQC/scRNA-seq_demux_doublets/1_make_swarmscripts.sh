e5e4456aa6109aa3ba7f908ddec88364fae83fbf
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-09-25 12:27:57 -0400

    Create 1_make_swarmscripts.sh

---
+++ ProcessingAndQC/scRNA-seq_demux_doublets/1_make_swarmscripts.sh
@@ -0,0 +1,89 @@
+# bash script to create swarm files, run in command-line
+
+# fastq_list.txt is a txt file of fastq sample names (not the names of the actual fastq files, just the sample names) separated by a line w/ no header
+
+SWARM_FILENAME=/data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/cellranger_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/amp_pd/transcriptomics/fastq_processing/cellranger/fastq_list.txt)
+JOB_SCRIPT=/data/ADRD/amp_pd/transcriptomics/fastq_processing/jobscripts/4_run_cellranger_swarm.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/cellbender_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/amp_pd/transcriptomics/fastq_processing/cellranger/fastq_list.txt)
+JOB_SCRIPT=/data/ADRD/amp_pd/transcriptomics/fastq_processing/jobscripts/8_run_cellbender_swarm.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/doubletdetection_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/amp_pd/transcriptomics/fastq_processing/cellranger/fastq_list.txt)
+JOB_SCRIPT=/data/ADRD/amp_pd/transcriptomics/fastq_processing/jobscripts/9_run_demuxafy_doubletdetection_swarm.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/scdblfinder_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/amp_pd/transcriptomics/fastq_processing/cellranger/fastq_list.txt)
+JOB_SCRIPT=/data/ADRD/amp_pd/transcriptomics/fastq_processing/jobscripts/10_run_demuxafy_scdblfinder_swarm.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/scds_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/amp_pd/transcriptomics/fastq_processing/cellranger/fastq_list.txt)
+JOB_SCRIPT=/data/ADRD/amp_pd/transcriptomics/fastq_processing/jobscripts/11_run_demuxafy_scds_swarm.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/demuxalot_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/amp_pd/transcriptomics/fastq_processing/cellranger/fastq_list.txt)
+JOB_SCRIPT=/data/ADRD/amp_pd/transcriptomics/fastq_processing/jobscripts/12_run_demuxafy_demuxalot_swarm.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/vireo_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/amp_pd/transcriptomics/fastq_processing/cellranger/fastq_list.txt)
+JOB_SCRIPT=/data/ADRD/amp_pd/transcriptomics/fastq_processing/jobscripts/13_run_demuxafy_vireo_swarm.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/demuxafy_combine_results.swarm
+FASTQ_LIST=$(cat /data/ADRD/amp_pd/transcriptomics/fastq_processing/cellranger/fastq_list.txt)
+JOB_SCRIPT=/data/ADRD/amp_pd/transcriptomics/fastq_processing/jobscripts/14_demuxafy_combine_results.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
